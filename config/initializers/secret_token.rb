265eda753e8903363e585e3d99ccd7e6fbea86e9
Ian Meikle <imeikle@gmail.com>
2013-12-31 12:34:06 +0000

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Howgoodami::Application.config.secret_key_base = 'ca5cb187e9a1b7cb674e0247706ab3aea4b30a804df55073cad3461a6997a874dcd7c69e6643bcd91f2a817dba07db8641c67505db8dfe6ae089a48e2d436596'
